d28f40ca0baa5bcd1a95a7b6cd4610cb436e8a77
takemoto-yosuke <takemoto.yosuke@coresite.ne.jp>
2022-08-02 01:43:24 +0000

    heroku-set2

--- database/migrations/2022_07_15_084724_create_estimates_table.php
+++ database/migrations/2022_07_15_084724_create_estimates_table.php
@@ -22,11 +22,11 @@ return new class extends Migration
             $table->string('unit')->nullable();
             $table->integer('unit_prise')->nullable();
             $table->integer('prise')->nullable();
-            $table->boolean('web_flag')->nullable();
-            $table->boolean('app_flag')->nullable();
+            $table->integer('web_flag')->nullable();
+            $table->integer('app_flag')->nullable();
             $table->string('machine_both')->nullable();            
-            $table->boolean('ja_flag')->nullable();
-            $table->boolean('eng_flag')->nullable();
+            $table->integer('ja_flag')->nullable();
+            $table->integer('eng_flag')->nullable();
             $table->string('lang_both')->nullable();
             $table->integer('checkitem_id');               
             $table->timestamps();
