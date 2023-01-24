791ddbe66be0eae9b6ae25e724679c7b0d38d5e1
takemoto-yosuke <takemoto.yosuke@coresite.ne.jp>
2023-01-24 07:27:20 +0000

    20230124-7

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -9,13 +9,16 @@
              <a href="{{ url('/first_estimates_make') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">初期見積作成</a>
          </div>         
          <div class="card-title">
-            <a href="{{ url('/checkitem') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">チェック項目一覧</a>
+<!--            <a href="{{ url('/checkitem') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">チェック項目一覧</a> -->
+            <a tabindex="-1" class="text-sm text-gray-700 dark:text-gray-500 underline">チェック項目一覧</a>
          </div>         
          <div class="card-title">
-            <a href="{{ url('/estimate') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">見積項目一覧</a>
+<!--            <a href="{{ url('/estimate') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">見積項目一覧</a> -->
+            <a tabindex="-1" class="text-sm text-gray-700 dark:text-gray-500 underline">見積項目一覧</a>
          </div>
          <div class="card-title">
-             <a href="{{ url('/category') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">カテゴリー一覧</a>
+<!--             <a href="{{ url('/category') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">カテゴリー一覧</a> -->
+             <a tabindex="-1" class="text-sm text-gray-700 dark:text-gray-500 underline">カテゴリー一覧</a>
          </div>         
      	    
      </div>
