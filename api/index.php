aa1420e596f0ea2e00b7f7f2c7c9ebc9e42c7b9a
yosuke <83564065+takemoto-yosuke@users.noreply.github.com>
2024-08-09 14:57:41 +0900

    Update index.php

--- api/index.php
+++ api/index.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+require __DIR__ . '/../public/index.php';
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
